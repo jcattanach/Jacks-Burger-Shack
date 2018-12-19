c55c7fdd1550a443804bdf8ba1c3ab1bb6e1f2ff
Jack Cattanach <jackcattanach@live.com>
2018-12-19 15:17:23 -0600

    added more styling to home page

--- client-side/src/components/Home.js
+++ client-side/src/components/Home.js
@@ -5,7 +5,11 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h1>Home</h1>
+        <h1>Welcome to Jack's Burger Shack!</h1>
+        <p>12345 Not Real St, FakePlace, USA, 55555</p>
+        <p>555-555-1234</p>
+        <img className="bigLogo" src={require('./imgs/logo-big.png')} />
+        <h4>To get started click on the build your burger tab in the menu!</h4>
       </div>
     );
   }
