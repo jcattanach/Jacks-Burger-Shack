804c5250f7b0f7a762841e89c2e7a9110eeb970c
Jack Cattanach <jackcattanach@live.com>
2018-12-21 14:18:59 -0600

    client-side style changes

--- client-side/src/components/Menu.js
+++ client-side/src/components/Menu.js
@@ -1,40 +1,34 @@
 import React, { Component } from 'react';
 import './Menu.css';
 import {Link} from 'react-router-dom'
-import Media from "react-media";
+import { connect } from 'react-redux'
 
 
 class Menu extends Component {
   render() {
+
+    let cart = <img className="cartIcon" src={require('./imgs/shopping-cart2.png')} />
+
+    if(this.props.crt.length >= 1){
+      cart = <img className="cartIcon" src={require('./imgs/shopping-cart-full2.png')} />
+    }
+
     return (
-          <Media query="(max-width: 485px)">
-            {matches =>
-              matches ? (
-            <div className="Menu2">
-              <img className="logo" src={require('./imgs/logo-2.png')} />
-                <div class="dropdown">
-                  <button class="dropbtn" onSelect={() => null}>Menu</button>
-                  <div class="dropdown-content">
-                    <Link to = "/">Home</Link>
-                    <Link to = "/build-burger">Build your Burger</Link>
-                    <Link to = "/checkout">Checkout</Link>
-                  </div>
-                </div>
-              </div>
-              ) : (
-            <div className="Menu">
-              <img className="logo" src={require('./imgs/logo-2.png')} />
-                <ul>
-                  <li><Link to = "/">Home</Link></li>
-                  <li><Link to = "/build-burger">Build your Burger</Link></li>
-                  <li><Link to = "/checkout">Checkout</Link></li>
-                </ul>
-              </div>
-              )
-            }
-          </Media>
+        <div className="Menu">
+            <ul className="navBar">
+              <li><Link to = "/"><img className="logo" src={require('./imgs/logo-2.png')} /></Link></li>
+              <li><Link to = "/build-burger"><div className="navMenuItem" >Build your Burger</div></Link></li>
+              <li><Link to = "/checkout">{cart}</Link></li>
+            </ul>
+          </div>
     );
   }
 }
 
-export default Menu;
+const mapStateToProps = (state) => {
+  return {
+    crt: state.cart
+  }
+}
+
+export default connect(mapStateToProps)(Menu);
